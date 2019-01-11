45a7766c1493739b1aa68ae77c80ad00869e7872
Monica Lent <lent.monica@gmail.com>
2019-01-11 11:54:21 +0100

    Change message.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Fragment, Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const Node = () => <div>Hello from CI!</div>;
+const Node = () => <div>Hello Felix!</div>;
 
 window.NodeComponent = Node;
