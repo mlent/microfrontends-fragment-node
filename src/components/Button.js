382fb9ad3c7a157cda6ef9d0dafb8e521000917b
Monica Lent <lent.monica@gmail.com>
2019-02-16 16:24:03 +0100

    Add padding to button.

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,6 +7,8 @@ const Button = styled('button')`
     color: ${theme.colors.text};
     border: 1px solid ${theme.colors.text};
     margin-top: ${theme.spacing.unit * 10}px;
+    margin-right: ${theme.spacing.unit}px;
+    padding: ${theme.spacing.unit}px ${theme.spacing.unit * 3}px;
 
     &:active {
       background-color: ${theme.colors.highlight};
